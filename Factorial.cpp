dc16f4580d4324c362e82c7abc970489ddb7ea62
AndrezQuezada <al215993@alumnos.uacj.mx>
2024-10-29 11:36:06 -0600

    Agregar cajero

--- Factorial.cpp
+++ Factorial.cpp
@@ -11,15 +11,42 @@ int factorial(int a) {
         factorials = factorials;
 
     }
-    cout << "numero factorial: " << factorials;
+    cout << "numero factorial: " << factorials << endl;
 
     return 0;
 }
+
+void consultar(int a) {
+
+
+    cout << a;
+
+}
+
+void retirar(int a) {
+
+    int retiro;
+    cout << "Ingrese la cantidad a retirar";
+    cin >> retiro;
+    int retirada = a - retiro;
+    cout << "Saldo Final" << retirada;
+
+}
+
+
 
 int main()
 {
     factorial(5);
+    int cuenta = 2000;
+
+    cout << "tu saldo es de: " << cuenta << endl;
+
+    consultar(cuenta);
+
+
+
+    retirar(cuenta);
 
     return 0;
 }
-
